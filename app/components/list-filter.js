46d1e30477f9a8b791c2f4f8381928e3dcff19d3
Gustavo Cruz <gustavo.cruz@soaexpert.com.br>
2017-09-27 13:56:35 -0300

    refactor: change rentals filter logic

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -6,14 +6,22 @@ export default Ember.Component.extend({
 
   init() {
     this._super(...arguments);
-    this.get('filter')('').then((results) => this.set('results', results));
+    this.get('filter')('').then((allResults) => {
+      this.set('results', allResults.results);
+    });
   },
 
   actions: {
     handleFilterEntry() {
       let filterInputValue = this.get('value');
       let filterAction = this.get('filter');
-      filterAction(filterInputValue).then((filterResults) => this.set('results', filterResults));
+      
+      filterAction(filterInputValue)
+      .then((filterResults) => {
+        if (filterResults.query === this.get('value')) {
+          this.set('results', filterResults.results);
+        }
+      });
     }
   }
 
